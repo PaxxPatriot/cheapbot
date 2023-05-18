7b5eda2df9bc78c6b9efd56caef32882faf0ea54
PaxxPatriot <34141101+PaxxPatriot@users.noreply.github.com>
2023-05-18 21:40:34 +0200

    Update cheapbot.py

--- cheapbot.py
+++ cheapbot.py
@@ -13,6 +13,9 @@ if config.discord_webhook_url == '':
 
 skinport_client = skinport.Client()
 
+async def get_item_from_sales_history(market_hash_name: str) -> skinport.ItemWithSales:
+    sales_history = await skinport_client.get_sales_history()
+    return [item for item in sales_history if item.market_hash_name == market_hash_name][0]
 
 @skinport_client.listen("saleFeed")
 async def on_sale_feed(data):
@@ -20,11 +23,19 @@ async def on_sale_feed(data):
     salefeed = skinport.SaleFeed(data=data)
     sales = salefeed.sales
     for s in sales:
-        if s.stattrak or "\u2605" not in s.tags:
+        if s.suggested_price < 5: # Ignore items which are suggested for less than 5 EUR
             return
-        sales = await skinport_client.get_sales_history(s.market_hash_name)
-        if sales[0].last_7_days.min is not None and sales[0].last_7_days.min > s.sale_price:
+
+        if "\u2605" in s.tags and s.stattrak: # Ignore StatTrak Knives
+            return
+        
+        item = await get_item_from_sales_history(s.market_hash_name)
+        if not item:
+            return
+
+        if (item.last_7_days.avg is not None and item.last_7_days.avg > s.sale_price):
             paginator.add_line(f"{s.market_hash_name}: {s.sale_price} {s.currency} Float: {s.wear}\n<{s.url}>")
+
     async with aiohttp.ClientSession() as session:
         webhook = discord.Webhook.from_url(config.discord_webhook_url, session=session)
         for page in paginator.pages:
@@ -32,5 +43,5 @@ async def on_sale_feed(data):
 
 
 if __name__ == "__main__":
-    print("Running...")
+    print(f"Running... on skinport.py {skinport.__version__}")
     skinport_client.run()
